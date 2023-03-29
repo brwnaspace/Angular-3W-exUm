aa72ad773e685f3df2b47f82f510d8ddb40ff503
brwnalima <bruna.lima@edu.unirio.br>
2023-03-29 11:20:01 -0300

    commit m e c

---
+++ thirdWeek/src/app/features/components/colaboradores/lista-colabs/lista-colabs.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-colabs',
+  templateUrl: './lista-colabs.component.html',
+  styleUrls: ['./lista-colabs.component.scss']
+})
+export class ListaColabsComponent {
+
+  title: string = 'Listagem de Colaboradores';
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
