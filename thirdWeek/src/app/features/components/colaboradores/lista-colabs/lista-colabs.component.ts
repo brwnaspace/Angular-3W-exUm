66f4e0ab7353860289d17db754dc397234a8202b
brwnalima <bruna.lima@edu.unirio.br>
2023-03-29 11:55:47 -0300

    listagem

--- thirdWeek/src/app/features/components/colaboradores/lista-colabs/lista-colabs.component.ts
+++ thirdWeek/src/app/features/components/colaboradores/lista-colabs/lista-colabs.component.ts
@@ -8,13 +8,51 @@ import { Component } from '@angular/core';
 export class ListaColabsComponent {
 
   title: string = 'Listagem de Colaboradores';
+  colab: Colabs[] = [];
 
-  constructor(){
+  /*
+  Ana: Colabs = {
+    id: 2, 
+    nome: 'Ana Letícia', 
+    cargo: 'Dev Pl', 
+    empresa: "MJV"
+  }
 
+  Nathan: Colabs = {
+    id: 7, 
+    nome: 'Nathan Carlos', 
+    cargo: 'Dev Pl', 
+    empresa: "MJV"
+  }
+
+  Bruna: Colabs = {
+    id: 1, 
+    nome: 'Bruna Lima', 
+    cargo: 'Dev Pl', 
+    empresa: "MJV"
+  }
+  */
+
+  constructor() {
+    this.colab = [
+      { id: 2, nome: 'Ana Letícia', cargo: 'Dev Pl', empresa: "MJV" },
+      { id: 7, nome: 'Nathan Carlos', cargo: 'Dev Pl', empresa: "MJV" },
+      { id: 1, nome: 'Bruna Lima', cargo: 'Dev Pl', empresa: "MJV" },
+      { id: 3, nome: 'Brenda Machado', cargo: 'Dev Pl', empresa: "MJV" },
+      { id: 4, nome: 'Maicon Brendon', cargo: 'Dev Pl', empresa: "MJV" },
+      { id: 5, nome: 'Tamires Porto', cargo: 'Dev Pl', empresa: "MJV" }
+    ];
   }
 
   ngOnInit(): void {
-    
+
   }
 
+}
+
+interface Colabs {
+  id: number;
+  nome: string;
+  cargo: string;
+  empresa: string;
 }
